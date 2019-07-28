a4b374689a29dd1840e2d188237d40a9b3005a14
ariflateefshah <arif@ideas2it.com>
2019-07-28 20:04:53 +0530

    Feature:  Serving Static Files HTML, CSS, Images  -- How to configure imagepath , csspath  -- How to read import html files and read them

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
 // Creating Web Server
 
 var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+
 
 // http.createServer(function(req , resp){   // Normal syntax 
 http.createServer((req , resp) => {    // ES 6 Syntax
-    resp.writeHead(200,{'content-type':'text/html'});
-    resp.write('Request came from Url' + req.url + '<br />');
-    resp.write('Hello Brother , How are You');
-    resp.end();
-}).listen(3000);
+    if(req.url === "/"){
+        fs.readFile('./public/index.html',"UTF-8",function(err , data){
+            resp.writeHead(200,{'content-type':'text/html'});
+            resp.end(data)
+        });
+    } else if(req.url.match("\.css$")){
+        var cssPath = path.join(__dirname,'public',req.url);
+        var fileStream = fs.createReadStream(cssPath ,"UTF-8");
+        resp.writeHead(200,{"ContentType":"text/css"});
+        fileStream.pipe(resp);
+    }else if(req.url.match("\.png$")){
+        var imagePath = path.join(__dirname,'public',req.url);
+        var fileStream = fs.createReadStream(imagePath);
+        resp.writeHead(200,{"ContentType":"image/png"});
+        fileStream.pipe(resp);
+    }else{
+        resp.writeHead(404, {"Content-Type":"text/html"});
+        resp.end("No Page Found");
+    }
+   
+}).listen(3003);
 
