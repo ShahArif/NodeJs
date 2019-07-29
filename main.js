b1c18f17acfea985efd2b5dbd396c60e121c34bb
ariflateefshah <arif@ideas2it.com>
2019-07-29 15:29:42 +0530

    Feature: Arrow functions in Node js

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
 console.log("Hello World");
 
-const a = 10;
-const b = 20;
-console.log("Addition of a and b = " ,a + b);
+var student = {
+   name:'arif Shah',
+   email: 'arif@gmail.com',
+   designation :'Senior Software Engineer',
+   company : 'Ideas2IT Technologies Pvt Ltd'
+};
 
+
+var getName = function(a , b){
+   console.log("Hey");
+   console.log("You have passed " + a + "and " + b );
+   var c = a + b ;
+   console.log("Sum of the given numbers is : " + c);
+}
+
+getName(5 , 6);
+
+// Arrrow Functions
+var getName1 = (a , b)=>{
+    console.log("Hey");
+    console.log("You have passed " + a + "and " + b );
+    var c = a + b ;
+    console.log("Sum of the given numbers is : " + c);
+ }
+ 
+ getName1(5 , 6);
