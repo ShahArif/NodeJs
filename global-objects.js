2afd74ba9f05417cabac0330af1d00efd7409709
ariflateefshah <arif@ideas2it.com>
2019-07-29 15:48:52 +0530

    Feature : Global Objects

---
+++ global-objects.js
@@ -0,0 +1,17 @@
+// __dirname
+//__filename
+// require
+// console
+// buffer
+// module
+// export
+
+
+console.log("this is the directory name :  " +__dirname);
+
+console.log("<br>")
+console.log("this is the File name :  " +__filename);
+
+var main = require('./main');
+
+console.log("My name is "+ main.name);
--- main.js
+++ main.js
@@ -1,28 +1,4 @@
-console.log("Hello World");
+console.log('This comes from main.js');
 
-var student = {
-   name:'arif Shah',
-   email: 'arif@gmail.com',
-   designation :'Senior Software Engineer',
-   company : 'Ideas2IT Technologies Pvt Ltd'
-};
-
-
-var getName = function(a , b){
-   console.log("Hey");
-   console.log("You have passed " + a + "and " + b );
-   var c = a + b ;
-   console.log("Sum of the given numbers is : " + c);
-}
-
-getName(5 , 6);
-
-// Arrrow Functions
-var getName1 = (a , b)=>{
-    console.log("Hey");
-    console.log("You have passed " + a + "and " + b );
-    var c = a + b ;
-    console.log("Sum of the given numbers is : " + c);
- }
- 
- getName1(5 , 6);
+var name = "arif"
+module.exports.name = name;
