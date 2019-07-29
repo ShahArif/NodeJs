cb2ca4cd6fa31b86293e0a137fc0cbdb1689f813
ariflateefshah <arif@ideas2it.com>
2019-07-29 16:52:29 +0530

    Feature : Nodemon | Publishes the changes wihtout restarting the server -- npm install -g nodemon

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-console.log('This comes from main.js');
+// Creating Web Server
+// To use the nodemon we have to install nodemon package 
+// npm install -g nodemon
 
-var name = "arif"
-module.exports.name = name;
+var http = require('http');
+http.createServer((req , resp) => {    // ES 6 Syntax
+    resp.writeHead(200,{'content-type':'text/html'});
+    resp.write('Request came from Url' + req.url + '<br />');
+    resp.write('Hello Brother , How are You');
+    resp.write('<br> Hey Node Mon is working dude');
+    resp.end();
+}).listen(3000);
