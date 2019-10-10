31034f5d1ea58bcf3cf0635d054d5c39a55a36bc
ariflateefshah <arif@ideas2it.com>
2019-10-10 22:40:02 +0530

    Feature: File System / reading/wrting file/ creating directory

--- main.js
+++ main.js
@@ -1,40 +1,41 @@
-var http = require('http');
+
+// Setting interval per 3 seconds
+// var time = 0;
+// var timer = setInterval(function(){
+//     console.log('hey Dude');
+//     time+=1;
+//     if(time >= 6){
+//         clearInterval(timer);
+//     }
+// },3000);
+
+// Setting time out after 3 seconds
+// setTimeout(function(){
+//     console.log('hey Dude');
+// },3000)
+
+
+// Directory
+
+// console.log(__dir , 'Directory');
+
+// Creating Directory
+
 var fs = require('fs');
+// Creating Directory
+fs.mkdirSync('Test', function(){
+  console.log('woreking');
+});
+// creating file inside the Directory
+  var content = 'hey read me';
+  fs.writeFileSync('./Test/test.txt',content);
+  fs.writeFileSync('test2.txt',content);
+
+var text = fs.readFileSync('./Test/test.txt','utf-8', function(err , data){
+    console.log(data);
+});
 
 
-const events = require('events');
-const  event = new events.EventEmitter();
+// remove Directory
 
-//  Approach 1
-// event.on('something',(param1, param2)=>{
-//     console.log('First Event called with '+ param1 + " Second arg as number is " + param2);
-// });
-
-// event.emit('something','This message is passed as parameter',25);
-
-
-// Approach 2
-const first_event = function(n1 , n2){
-    console.log('First Event ' , n1+n2);
-    event.emit('multiply',2,5);
-}
-
-const second_event = function(n1 , n2){
-    console.log('Second Event' + n1*n2);
-    
-}
-
-event.on('add',first_event);
-event.on('multiply',second_event);
-event.emit('add',2,5);
-
-
-
-// http.createServer(function(req , resp){   // Normal syntax 
-http.createServer((req , resp) => {    // ES 6 Syntax
-    resp.writeHead(200,{'content-type':'text/html'});
-    resp.write('Request came from Url' + req.url + '<br />');
-    resp.write('Hello Brother , How are You');
-    resp.end();
-}).listen(3000);
-
+fs.unlink('test2.txt');
