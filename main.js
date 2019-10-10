6eb654a3988581dc78a01248198226faf8f25b03
ariflateefshah <arif@ideas2it.com>
2019-10-11 00:10:44 +0530

    Feature: Express starting | basic routing

--- main.js
+++ main.js
@@ -1,41 +1,23 @@
+var express = require('express');
 
-// Setting interval per 3 seconds
-// var time = 0;
-// var timer = setInterval(function(){
-//     console.log('hey Dude');
-//     time+=1;
-//     if(time >= 6){
-//         clearInterval(timer);
-//     }
-// },3000);
-
-// Setting time out after 3 seconds
-// setTimeout(function(){
-//     console.log('hey Dude');
-// },3000)
+var app = express();
 
 
-// Directory
-
-// console.log(__dir , 'Directory');
-
-// Creating Directory
-
-var fs = require('fs');
-// Creating Directory
-fs.mkdirSync('Test', function(){
-  console.log('woreking');
+app.get('/' , function(req , resp ){
+    resp.send('this is home page');
 });
-// creating file inside the Directory
-  var content = 'hey read me';
-  fs.writeFileSync('./Test/test.txt',content);
-  fs.writeFileSync('test2.txt',content);
 
-var text = fs.readFileSync('./Test/test.txt','utf-8', function(err , data){
-    console.log(data);
+app.get('/blog' , function(req , resp ){
+    resp.send('this is Blog page');
+});
+
+app.get('/post/:id' , function(req , resp ){
+    resp.send('this is post id ' + req.params.id);
+});
+
+app.get('/contact' , function(req , resp ){
+    resp.send('this is Contact page');
 });
 
 
-// remove Directory
-
-fs.unlink('test2.txt');
+app.listen(3002);
