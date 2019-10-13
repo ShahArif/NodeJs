b2da0432531510fd3ec930c5ec16a213b81b39a0
ariflateefshah <arif@ideas2it.com>
2019-10-13 17:32:34 +0530

    Feature: Todo App | 3 | Connecting mongodb | Incomplete

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -5,15 +5,45 @@ data = [
 ];
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({extended:false});
+var mongo = require('mongodb');
+var assert = require('assert');
+
+var url = 'mongodb://localhost:27017/todos';
 
 
 module.exports = function(app){
 
   app.get('/todo', function(req , res){
-      res.render('todo',{todos:data});
+    var resultArray =[];
+    mongo.connect(url, {useUnifiedTopology: true}, function(err , db){
+      assert.equal(null , err);
+      var cursor = db.collection('mytodos').find();
+      cursor.forEach(function(doc, err){
+          assert.equal(null,err);
+          resultArray.push(doc);
+      },function(){
+          db.close();
+          res.render('todo',{todos:resultArray});
+      });
+    })
+
   });
 
   app.post('/todo' ,urlencodedParser ,function(req , res){
+      var item ={
+        title:req.body.title
+      };
+
+      mongo.connect(url, {useUnifiedTopology: true}, function(err , db){
+        assert.equal(null , err);
+        db.collection('mytodos').insertOne(item , function(err , result){
+          assert.equal(null, err);
+          console.log('item inserted');
+          db.close();
+        });
+      })
+
+
     data.push(req.body);
     res.json(data);
   });
